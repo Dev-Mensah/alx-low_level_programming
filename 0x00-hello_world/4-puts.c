3ca5202a26d76e4b533613becc5bc34e4d5f83eb
Albert Mensah <albertoforimensah04@gmail.com>
2023-06-16 11:54:12 -0700

    first c script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * return always 0 (success)
+ * Return: the main function will return an integer
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
