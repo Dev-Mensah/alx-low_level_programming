042fe122f9181a9e938099a1691d234de9d0b3a6
Albert Mensah <albertoforimensah04@gmail.com>
2023-06-18 06:53:34 -0700

    negatives added

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,21 +17,27 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 printf("Last digit of %d is ", n);
-lastDigit = abs(n) % 10;
+lastDigit = abs(n % 10); // Calculate the absolute value of the last digit
 printf("%d ", lastDigit);
 
-if (lastDigit > 5)
+if (n < 0)
 {
-printf("and is greater than 5");
+    printf("and is less than 6 and not 0");
+}
+else if (lastDigit > 5)
+{
+    printf("and is greater than 5");
 }
 else if (lastDigit == 0)
 {
-printf("and is 0");
+    printf("and is 0");
 }
 else
 {
-printf("and is less than 6 and not 0");
+    printf("and is less than 6 and not 0");
 }
 printf("\n");
+
 return (0);
 }
+
