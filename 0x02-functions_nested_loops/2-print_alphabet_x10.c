932d6b1bef71646abcb3d1c87be4bbd03db1ad03
Albert Mensah <albertoforimensah04@gmail.com>
2023-06-20 13:05:51 -0700

    resolved

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,19 +6,19 @@
 
 void print_alphabet_x10(void)
 {
-char letter = 'a';
 int count = 0;
 
 while (count < 10)
 {
+char letter = 'a';
+
 while (letter <= 'z')
 {
 _putchar(letter);
 letter++;
 }
-count++;
-letter = 'a';
-}
 
 _putchar('\n');
+count++;
+}
 }
