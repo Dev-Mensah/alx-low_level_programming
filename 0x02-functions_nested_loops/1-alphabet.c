cf7377c7e48bb1ec25e7dd196b1be28b22bd2465
Albert Mensah <albertoforimensah04@gmail.com>
2023-06-20 12:41:33 -0700

    push

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints in lowercase
+ */
+
+void print_alphabet(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+
+_putchar('\n');
+}
