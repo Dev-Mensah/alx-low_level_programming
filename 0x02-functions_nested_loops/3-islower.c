e0089a2cfc337ab479fa21eaf92d1d9c74ebd72c
Albert Mensah <albertoforimensah04@gmail.com>
2023-06-21 06:40:48 -0700

    push

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,23 @@
+/*
+ * File: 3-islower.c
+ */
+
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _islower - print lowercase
+ * @c: the number to be printed
+ * return: 0
+ * _islower - return: 1
  */
-int main(void)
+int _islower(int c)
 {
-int r;
-r = _islower('H');
-_putchar(r + '0');
-r = _islower('o');
-_putchar(r + '0');
-r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
-return (0);
+if (c >= 'a' && c <= 'z')
+{
+_putchar(1);
+}
+else
+{
+_putchar(0);
+}
 }
