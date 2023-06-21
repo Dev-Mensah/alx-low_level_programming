4be987fdbc9d095a4823cca88b14aa7ec966f891
Albert Mensah <albertoforimensah04@gmail.com>
2023-06-21 07:16:58 -0700

    prints the sign of a number.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,15 +12,15 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-printf("+%d\n", 1);
+return (+1)
 }
 else if (n == 0)
 {
-printf("%d\n", 0);
+return (0)
 }
 else
 {
-printf("%d\n", 1);
+return (-1)
 }
 }
 
