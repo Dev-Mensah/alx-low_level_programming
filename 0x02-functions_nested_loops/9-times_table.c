3a51c7e7b6b5b50a5a269f48cfcc1a071c601544
Albert Mensah <albertoforimensah04@gmail.com>
2023-06-21 11:35:35 -0700

    9-times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,13 +6,22 @@
 */
 void times_table(void)
 {
-int i, j;
+int i, j, result;
 
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
+result = i * j
+if (result <= 9)
+{
+printf(" ")
 printf("%d, ", i * j);
+}
+else
+{
+printf("%d, ", i * j);
+}
 }
 printf("\n");
 }
