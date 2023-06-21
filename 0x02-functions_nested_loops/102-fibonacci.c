0cf09b34f3cef0b96e93e8ea27204fb4e57e7ddd
Albert Mensah <albertoforimensah04@gmail.com>
2023-06-21 12:33:41 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,14 +12,15 @@
 
 void print_fibonacci(int n)
 {
-int fib1 = 1, fib2 = 2, fib_next, i;
+unsigned long long fib1 = 1, fib2 = 2, fib_next;
+int i;
 
-printf("%d, %d, ", fib1, fib2);
+printf("%llu, %llu, ", fib1, fib2);
 
 for (i = 3; i <= n; i++)
 {
 fib_next = fib1 + fib2;
-printf("%d", fib_next);
+printf("%llu", fib_next);
 
 if (i < n)
 {
@@ -37,8 +38,9 @@ int main(void)
 {
 int n = 50;
 
-printf("The first %d Fibonacci numbers are: \n", n);
+printf("The first %d Fibonacci numbers are:\n", n);
 print_fibonacci(n);
 
 return (0);
 }
+
