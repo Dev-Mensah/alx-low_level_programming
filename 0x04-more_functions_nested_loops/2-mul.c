32f2349a0a22b058ef09a9336c6ac3a642303640
Albert Mensah <albertoforimensah04@gmail.com>
2023-06-22 12:48:46 -0700

    ready

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/*
+* File: 2-mul.c
+*/
+
+#include "main.h"
+
+/**
+* mul - Checks if a character is lowercase
+* @a: integer to be multiplied
+* @b: integer to be multiplied
+*
+* Return: result
+*/
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
