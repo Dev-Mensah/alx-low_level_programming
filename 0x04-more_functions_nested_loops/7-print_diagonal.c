87996c137ce0a0f96b540cfcb0a501e26500ad87
Albert Mensah <albertoforimensah04@gmail.com>
2023-06-23 07:12:16 -0700

    diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,16 +14,23 @@
 void print_diagonal(int n)
 {
 int i;
+int j;
 if (n > 0)
 {
 for (i = 0; i < n; i++)
 {
-_putchar('\\');
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
 }
+_putchar('\\');
 _putchar('\n');
+}
 }
 else
 {
 _putchar('\n');
+return;
 }
+_putchar('\n');
 }
