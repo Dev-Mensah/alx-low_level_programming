df99d2165081b5ddb7e51bfe64314b0641b33c3c
Albert Mensah <albertoforimensah04@gmail.com>
2023-06-23 07:01:10 -0700

    pushing

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,14 +14,16 @@
 void print_line(int n)
 {
 int i;
-if (n <= 0)
+if (n > 0)
 {
-_putchar('\n');
-return ();
-}
 for (i = 0; i < n; i++)
 {
 _putchar('_');
 }
 _putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
 }
