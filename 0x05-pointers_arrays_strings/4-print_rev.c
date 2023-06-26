79469fe0fecdcaae45235e80a4354f1c6173f9ee
Albert Mensah <albertoforimensah04@gmail.com>
2023-06-26 11:48:09 -0700

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_rev - print string in reverse to stdout
+* @s: string input
+* _putchar: result, otherwise
+*/
+
+void print_rev(char *s)
+{
+int len = 0;
+while (str[length] != '\0')
+{
+length++;
+}
+for (int i = length - 1; i >= 0; i--)
+{
+_putchar(str[i]);
+}
+
+_putchar('\n');
+}
