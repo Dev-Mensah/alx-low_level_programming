8ff0e3290502efa46568449c780e5844702ff097
Albert Mensah <albertoforimensah04@gmail.com>
2023-06-26 11:30:18 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* reset_to_98 - function that takes a pointer 
+* @n: interger
+* _putchar: result, otherwise
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
