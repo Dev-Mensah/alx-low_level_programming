2b44d03210897855c45b060004a6a5b45c697d1e
Albert Mensah <albertoforimensah04@gmail.com>
2023-07-11 08:29:10 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main -  main function
+* @argc: int
+* @argv: int
+* Return: result, otherwise
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+
+return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main -  main function
+* @argc: int
+* @argv: int
+* Return: result, otherwise
+*/
+
+int main(int argc, char *argv[])
+{
+(void) argv; /*Ignore argv*/
+printf("%d\n", argc - 1);
+
+return (0);
+}
