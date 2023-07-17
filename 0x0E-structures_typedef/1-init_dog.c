d8c37b93d6d408091edb9c1c8cfcbe67806a9833
Albert Mensah <albertoforimensah04@gmail.com>
2023-07-17 08:19:18 -0700

    first commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* struct dog - a dog's basic info
+* @name: First param
+* @d: pointer
+* @age: Second param
+* @owner: Third param
+*
+* Description: Extra details
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d == NULL)
+d = malloc(sizeof(struct dog));
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
